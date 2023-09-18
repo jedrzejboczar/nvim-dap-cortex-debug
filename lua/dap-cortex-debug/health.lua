d35e62d89031ca4082f3fa8f7675b9b29acca4a8
Jędrzej Boczar <yendreij@gmail.com>
2023-09-18 22:00:39 +0200

    fix(health): test for `== 1` when using vim.fn as 0 is truthy in lua

--- lua/dap-cortex-debug/health.lua
+++ lua/dap-cortex-debug/health.lua
@@ -6,7 +6,7 @@ local utils = require('dap-cortex-debug.utils')
 function M.check()
     vim.health.report_start('nvim-dap-cortex-debug')
 
-    if vim.fn.executable(config.node_path) then
+    if vim.fn.executable(config.node_path) == 1 then
         local ok, version = pcall(vim.fn.system, 'node --version')
         if ok then
             vim.health.report_ok('Node.js installed: ' .. vim.trim(version))
@@ -18,11 +18,11 @@ function M.check()
     end
 
     local extension_path = utils.get_extension_path()
-    if extension_path and vim.fn.isdirectory(extension_path) then
+    if extension_path and vim.fn.isdirectory(extension_path) == 1 then
         vim.health.report_ok('cortex-debug extension found')
 
         local debugadapter_path = utils.get_debugadapter_path(extension_path)
-        if vim.fn.filereadable(debugadapter_path) then
+        if vim.fn.filereadable(debugadapter_path) == 1 then
             vim.health.report_ok('Found debugadapter.js')
         else
             vim.health.report_error('debugadapter.js not found: ' .. debugadapter_path)
