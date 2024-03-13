780897cb84db74794f27404188eea720631e4913
Jędrzej Boczar <yendreij@gmail.com>
2024-03-13 21:34:43 +0100

    feat(health): stop using deprecated api

--- lua/dap-cortex-debug/health.lua
+++ lua/dap-cortex-debug/health.lua
@@ -4,33 +4,33 @@ local config = require('dap-cortex-debug.config')
 local utils = require('dap-cortex-debug.utils')
 
 function M.check()
-    vim.health.report_start('nvim-dap-cortex-debug')
+    vim.health.start('nvim-dap-cortex-debug')
 
     if vim.fn.executable(config.node_path) == 1 then
         local ok, version = pcall(vim.fn.system, 'node --version')
-        if ok then
-            vim.health.report_ok('Node.js installed: ' .. vim.trim(version))
+        if ok and version then
+            vim.health.ok('Node.js installed: ' .. vim.trim(version))
         else
-            vim.health.report_error('Node.js executable but `node --version` failed')
+            vim.health.error('Node.js executable but `node --version` failed')
         end
     else
-        vim.health.report_error('Node.js not installed')
+        vim.health.error('Node.js not installed')
     end
 
     local extension_path = utils.get_extension_path()
     if extension_path and vim.fn.isdirectory(extension_path) == 1 then
-        vim.health.report_ok('cortex-debug extension found')
+        vim.health.ok('cortex-debug extension found: ' .. extension_path)
 
         local debugadapter_path = utils.get_debugadapter_path(extension_path)
         if vim.fn.filereadable(debugadapter_path) == 1 then
-            vim.health.report_ok('Found debugadapter.js')
+            vim.health.ok('Found debugadapter.js: ' .. debugadapter_path)
         else
-            vim.health.report_error('debugadapter.js not found: ' .. debugadapter_path)
+            vim.health.error('debugadapter.js not found: ' .. debugadapter_path)
         end
     elseif extension_path then
-        vim.health.report_error('cortex-debug extension path not a directory: ' .. extension_path)
+        vim.health.error('cortex-debug extension path not a directory: ' .. extension_path)
     else
-        vim.health.report_error('cortex-debug extension not found')
+        vim.health.error('cortex-debug extension not found')
     end
 end
 
